c9c397239fcc540b74d4f92c28ec795f04de86d5
Bishnu Nath <bishnu1324@gmail.com>
2021-11-02 12:49:00 +0600

    48-5 added a button and style it

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -13,6 +13,7 @@ const Product = (props) => {
         <p><small>by: {seller}</small></p>
         <p>Price: {price}</p>
         <p><small>only {stock} left in stock - order soon</small></p>
+        <button className='btn-regular'>add to cart</button>
       </div>
     </div>
   );
