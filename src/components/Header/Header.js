4e58f0bcf0278cf6d9727f47c75bfb0a2ccb7ecd
Bishnu Nath <bishnu1324@gmail.com>
2021-11-02 11:34:28 +0600

    48-2 Header Component, logo position, set nav menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is Header</h1>
+    <div className='header'>
+      <img className='logo' src={logo} alt="" />
+      <nav><a href="/shop">Shop</a><a href="orders">Order Review</a><a href="inventory">Manage Inventory</a></nav>
     </div>
   );
 };
