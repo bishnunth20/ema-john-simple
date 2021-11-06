429387b1d1fa2d435303c787c4078233db01b538
Bishnu Nath <bishnu1324@gmail.com>
2021-11-06 10:40:09 +0600

    48-8 Install external packages, react-fontawesome, display icon

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
@@ -14,7 +16,7 @@ const Product = (props) => {
         <p><small>by: {seller}</small></p>
         <p>Price: {price}</p>
         <p><small>only {stock} left in stock - order soon</small></p>
-        <button onClick={() => props.handleAddToCart(props.product)} className='btn-regular'>add to cart</button>
+        <button onClick={() => props.handleAddToCart(props.product)} className='btn-regular'><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
       </div>
     </div>
   );
