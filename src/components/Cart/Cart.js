c87726fed2a32adada26328718bec41dcaadd078
Bishnu Nath <bishnu1324@gmail.com>
2021-11-06 10:53:35 +0600

    used reduce function & calculate product price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 const Cart = (props) => {
   const {cart} = props;
-  let total = 0;
-  for (const product of cart) {
-    total = total + product.price;
-  }
+
+  const total = cart.reduce((previous, product) => previous + product.price, 0);
+  // let total = 0;
+  // for (const product of cart) {
+  //   total = total + product.price;
+  // }
+  const shipping = 15;
+  const tax = (total + shipping) / 10;
+  const grandTotal = total + shipping + tax;
   return (
     <div>
       <h2>Order Summary</h2>
       <h4>Items Ordered: {props.cart.length}</h4>
       <br />
-      <p>Total: {total}</p>
+      <p>Total: {total.toFixed(2)}</p>
+      <p>Shipping: {shipping}</p>
+      <p>Tax: {tax.toFixed(2)}</p>
+      <p>Grand Total: {grandTotal.toFixed(2)}</p>
     </div>
   );
 };
